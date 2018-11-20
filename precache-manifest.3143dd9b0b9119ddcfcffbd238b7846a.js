a077e5444b8fa8d0667a84b94b570829e4b616fb
Marcelo Jorge Vieira <metal@alucinados.com>
2018-11-20 19:12:41 -0200

    Updates

---
+++ precache-manifest.3143dd9b0b9119ddcfcffbd238b7846a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "38365d14327652b247cc",
+    "url": "/politicos-react/static/js/runtime~main.38365d14.js"
+  },
+  {
+    "revision": "e5d458e9ea8bbbdafa29",
+    "url": "/politicos-react/static/js/main.e5d458e9.chunk.js"
+  },
+  {
+    "revision": "28e36c13da5439cf9996",
+    "url": "/politicos-react/static/js/1.28e36c13.chunk.js"
+  },
+  {
+    "revision": "e5d458e9ea8bbbdafa29",
+    "url": "/politicos-react/static/css/main.b2a35f76.chunk.css"
+  },
+  {
+    "revision": "864cfedbc5e87e46613eb32279656932",
+    "url": "/politicos-react/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6e64d2d759898d69d1e8788cefe91ebe.js"
+  "/politicos-react/precache-manifest.3143dd9b0b9119ddcfcffbd238b7846a.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/politicos-react/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
